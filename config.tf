38f180e0c3cdd166bd80751fab73f91a5b3d0097
Jason Plum <jplum@gitlab.com>
2017-05-18 18:22:10 +0000

    Add GitLab Runner to Terraform GCE

--- config.tf
+++ config.tf
@@ -67,8 +67,7 @@ variable "config_file" {
 }
 
 variable "dns_name" {
-    description = "The dns name of the instance to launch. Assumes the zone is part of the google managed zones"
-    default = "no_dns"
+    description = "The DNS name of the GitLab instance."
 }
 
 variable "dns_zone" {
@@ -93,4 +92,48 @@ variable "ssl_key" {
 variable "ssl_certificate" {
     description = "The SSL certificate file to use"
     default = "/dev/null"
+}
+
+variable "deploy_gitlab" {
+    description = "Enable / Disable deploying a GitLab instance"
+    default = true
+}
+
+variable "initial_root_password" {
+    description = "Set the initial admin password, generated if not provided"
+    default = "GENERATE"
+}
+
+variable "prefix" {
+    description = "Prefix to resource names in cloud provider"
+}
+
+variable "runner_count" {
+    description = "Number of GitLab CI Runners to create."
+    default = 0
+}
+
+variable "runner_host" {
+    description = "URL of the GitLab server Runner will register with"
+    default = "GENERATE"
+}
+
+variable "runner_token" {
+    description = "GitLab CI Runner registration token. Will be generated if not provided"
+    default = "GENERATE"
+}
+
+variable "runner_disk_size" {
+    description = "Size of disk (in GB) for Runner instances"
+    default = 20
+}
+
+variable "runner_image" {
+    description = "The Docker image a GitLab CI Runner will use by default"
+    default = "ruby:2.3"
+}
+
+variable "runner_machine_type" {
+    description = "A machine type for your compute instance, used by GitLab CI Runner"
+    default = "n1-standard-1"
 }
