79f7082f3f623cbbdea1c8467058891c6ff93b8a
Ian Baum <ibaum@gitlab.com>
2017-05-22 14:36:05 -0500

    Wrapping interpolation of random data in format function call
    We seem to be hitting https://github.com/hashicorp/terraform/issues/14399

--- gitlab.tf
+++ gitlab.tf
@@ -51,8 +51,8 @@ data "template_file" "gitlab" {
     template = "${file("${path.module}/templates/gitlab.rb.append")}"
 
     vars {
-        initial_root_password = "${var.initial_root_password != "GENERATE" ? var.initial_root_password : random_id.initial_root_password.hex}"
-        runner_token = "${var.runner_token != "GENERATE" ? var.runner_token : random_id.runner_token.hex}"
+        initial_root_password = "${var.initial_root_password != "GENERATE" ? var.initial_root_password : format("%s", random_id.initial_root_password.hex)}"
+        runner_token = "${var.runner_token != "GENERATE" ? var.runner_token : format("%s", random_id.runner_token.hex)}"
     }
 }
 
